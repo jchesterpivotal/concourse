743d22eda27dfc2fc37164c4bb46c6bef19244c8
Jamie Klassen <cklassen@pivotal.io>
2020-04-16 09:20:26 -0400

    atc: structure: standardize concurrent terminology
    concourse/concourse#5421
    
    I had a bad feeling about the slight inconsistency between the terms
    'concurrency limit' and 'concurrent request limit' and chose the later because
    it is more specific -- indeed, there are several 'max in flight'-type settings
    throughout Concourse. All of them could meaningfully be described as
    'concurrency limits', but this is the only one that has specifically to do with
    API requests.
    
    Signed-off-by: Jamie Klassen <cklassen@pivotal.io>

--- atc/atccmd/command.go
+++ atc/atccmd/command.go
@@ -1763,7 +1763,7 @@ func (cmd *RunCommand) constructAPIHandler(
 		logger,
 	)
 	apiWrapper := wrappa.MultiWrappa{
-		wrappa.NewConcurrencyLimitsWrappa(logger, cmd.concurrentRequestPolicy()),
+		wrappa.NewConcurrentRequestLimitsWrappa(logger, cmd.concurrentRequestPolicy()),
 		wrappa.NewAPIMetricsWrappa(logger),
 		wrappa.NewAPIAuthWrappa(
 			checkPipelineAccessHandlerFactory,
